98471af2e2eed0dff0c47c26ab36b4dcf86729df
Mark <bf-109g2@hotmail.com>
2021-10-04 16:36:33 -0500

    Reading float & scientific notation literals

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,142 +1,164 @@
+import { BigNumber } from 'bignumber.js'
 import { parse } from '../src/main';
 
 describe('parsing literals', () => {
   it('parse integer', () => {
-    const { result } = parse('69');
-    expect(result).toStrictEqual([69]);
+    let { result } = parse('69');
+    expect(result).toStrictEqual([new BigNumber(69)]);
   });
 
   it('parse negative integer', () => {
-    const { result } = parse('-69');
-    expect(result).toStrictEqual([-69]);
+    let { result } = parse('-69');
+    expect(result).toStrictEqual([new BigNumber(-69)]);
   });
 
-  // it('parse float', () => {
-  //   const { result } = parse('6.9');
-  //   expect(result).toStrictEqual([6.9]);
-  // });
+  it('parse float', () => {
+    const { result: res1 } = parse('6.9');
+    expect(res1).toStrictEqual([new BigNumber(6.9)]);
+    const { result: res2 } = parse('69.69');
+    expect(res2).toStrictEqual([new BigNumber(69.69)]);
+  });
 
-  // it('parse float 2', () => {
-  //   const { result } = parse('69.69');
-  //   expect(result).toStrictEqual([69.69]);
-  // });
-
-  // it('parse negative float', () => {
-  //   const { result } = parse('-3.14');
-  //   expect(result).toStrictEqual([-3.14]);
-  // });
+  it('parse scientific', () => {
+    const { result: res1 } = parse('1.23e5');
+    expect(res1).toStrictEqual([new BigNumber(1.23e5)]);
+    const { result: res2 } = parse('1.23E5');
+    expect(res2).toStrictEqual([new BigNumber(1.23e5)]);
+    const { result: res3 } = parse('1.23e-5');
+    expect(res3).toStrictEqual([new BigNumber(1.23e-5)]);
+    const { result: res4 } = parse('1.23E-5');
+    expect(res4).toStrictEqual([new BigNumber(1.23e-5)]);
+  });
 });
 
 describe('addition expression', () => {
   it('two integers with spaces in between', () => {
     const { result: res1 } = parse('1 + 2');
-    expect(res1).toStrictEqual([3]);
+    expect(res1).toStrictEqual([new BigNumber(3)]);
     const { result: res2 } = parse('1 - 2');
-    expect(res2).toStrictEqual([-1]);
+    expect(res2).toStrictEqual([new BigNumber(-1)]);
   });
 
   it('two integers with no spaces in between', () => {
     const { result: res1 } = parse('1+2');
-    expect(res1).toStrictEqual([3]);
+    expect(res1).toStrictEqual([new BigNumber(3)]);
     const { result: res2 } = parse('1-2');
-    expect(res2).toStrictEqual([-1]);
+    expect(res2).toStrictEqual([new BigNumber(-1)]);
   });
 
   it('two negative integers', () => {
     const { result: res1 } = parse('-1 + -2');
-    expect(res1).toStrictEqual([-3]);
+    expect(res1).toStrictEqual([new BigNumber(-3)]);
     const { result: res2 } = parse('-1 - -2');
-    expect(res2).toStrictEqual([1]);
+    expect(res2).toStrictEqual([new BigNumber(1)]);
   });
 
   it('positive and negative integer', () => {
     const { result: res1 } = parse('1 + -2');
-    expect(res1).toStrictEqual([-1]);
+    expect(res1).toStrictEqual([new BigNumber(-1)]);
     const { result: res2 } = parse('1 - -2');
-    expect(res2).toStrictEqual([3]);
+    expect(res2).toStrictEqual([new BigNumber(3)]);
   });
 
   it('negative and positive integer', () => {
     const { result: res1 } = parse('-1 + 2');
-    expect(res1).toStrictEqual([1]);
+    expect(res1).toStrictEqual([new BigNumber(1)]);
     const { result: res2 } = parse('-1 - 2');
-    expect(res2).toStrictEqual([-3]);
+    expect(res2).toStrictEqual([new BigNumber(-3)]);
   });
 });
 
 describe('multiplication expression', () => {
   it('two integers with spaces in between', () => {
     const { result: res1 } = parse('1 * 2');
-    expect(res1).toStrictEqual([2]);
+    expect(res1).toStrictEqual([new BigNumber(2)]);
     const { result: res2 } = parse('1 / 2');
-    expect(res2).toStrictEqual([0.5]);
+    expect(res2).toStrictEqual([new BigNumber(0.5)]);
   });
 
   it('two integers with no spaces in between', () => {
     const { result: res1 } = parse('1*2');
-    expect(res1).toStrictEqual([2]);
+    expect(res1).toStrictEqual([new BigNumber(2)]);
     const { result: res2 } = parse('1/2');
-    expect(res2).toStrictEqual([0.5]);
+    expect(res2).toStrictEqual([new BigNumber(0.5)]);
   });
 
   it('two negative integers', () => {
     const { result: res1 } = parse('-1 * -2');
-    expect(res1).toStrictEqual([2]);
+    expect(res1).toStrictEqual([new BigNumber(2)]);
     const { result: res2 } = parse('-1 / -2');
-    expect(res2).toStrictEqual([0.5]);
+    expect(res2).toStrictEqual([new BigNumber(0.5)]);
   });
 
   it('positive and negative integer', () => {
     const { result: res1 } = parse('1 * -2');
-    expect(res1).toStrictEqual([-2]);
+    expect(res1).toStrictEqual([new BigNumber(-2)]);
     const { result: res2 } = parse('1 / -2');
-    expect(res2).toStrictEqual([-0.5]);
+    expect(res2).toStrictEqual([new BigNumber(-0.5)]);
   });
 
   it('negative and positive integer', () => {
     const { result: res1 } = parse('-1 * 2');
-    expect(res1).toStrictEqual([-2]);
+    expect(res1).toStrictEqual([new BigNumber(-2)]);
     const { result: res2 } = parse('-1 / 2');
-    expect(res2).toStrictEqual([-0.5]);
+    expect(res2).toStrictEqual([new BigNumber(-0.5)]);
   });
 });
 
 describe('negation expression', () => {
   it('single negative integer', () => {
     const { result } = parse('-3102');
-    expect(result).toStrictEqual([-3102]);
+    expect(result).toStrictEqual([new BigNumber(-3102)]);
   })
 });
 
 describe('power expression', () => {
   it('two integers with spaces in between', () => {
     const { result } = parse('2 ^ 3');
-    expect(result).toStrictEqual([8]);
+    expect(result).toStrictEqual([new BigNumber(8)]);
   });
 
   it('two integers with no spaces in between', () => {
     const { result } = parse('2^3');
-    expect(result).toStrictEqual([8]);
+    expect(result).toStrictEqual([new BigNumber(8)]);
   });
 
   it('two negative integers', () => {
     const { result } = parse('-2^-3');
-    expect(result).toStrictEqual([-0.125]);
+    expect(result).toStrictEqual([new BigNumber(-0.125)]);
   });
 
   it('positive and negative integer', () => {
     const { result } = parse('2^-3');
-    expect(result).toStrictEqual([0.125]);
+    expect(result).toStrictEqual([new BigNumber(0.125)]);
   });
 
   it('negative and positive integer', () => {
     const { result } = parse('-2^3');
-    expect(result).toStrictEqual([-8]);
+    expect(result).toStrictEqual([new BigNumber(-8)]);
   });
 
   it('three integers', () => {
     const { result } = parse('2 ^ 3 ^ 4');
-    expect(result).toStrictEqual([4096]);
+    expect(result).toStrictEqual([new BigNumber(4096)]);
+  });
+
+  it('float as exponent', () => {
+    const { result } = parse('4 ^ (3 / 2)');
+    expect(result).toStrictEqual([new BigNumber(8)]);
   });
 });
 
+describe('order of precedence', () => {
+  it('testcase 1', () => {
+    const input = '4 + 5 * 4^-1^(1/2)';
+    const { result } = parse(input);
+    expect(result).toStrictEqual([new BigNumber(6.5)]);
+  });
+
+  it('testcase 2', () => {
+    const input = '4 + 5 * 4^-1^(1/2)';
+    const { result } = parse(input);
+    expect(result).toStrictEqual([new BigNumber(6.5)]);
+  });
+});
--- src/grammar.ts
+++ src/grammar.ts
@@ -1,36 +1,62 @@
+import BigNumber from 'bignumber.js';
 import { createToken, tokenMatcher, Lexer, CstParser } from 'chevrotain';
 
 const NewLine = createToken({
-  name: "NewLine",
+  name: 'NewLine',
   pattern: /\n|\r\n?/,
-})
+});
 
 // marking WhiteSpace as 'SKIPPED' makes the lexer skip it.
 const WhiteSpace = createToken({
-  name: "WhiteSpace",
+  name: 'WhiteSpace',
   pattern: /\s+/,
-  group: Lexer.SKIPPED
+  group: Lexer.SKIPPED,
 });
 
-const AssignOperator = createToken({name: "AssignOperator", pattern: /=/});
-const Variable = createToken({name: "Variable", pattern: /[a-zA-Z]\w*/});
+const AssignOperator = createToken({ name: 'AssignOperator', pattern: /=/ });
+const Variable = createToken({ name: 'Variable', pattern: /[a-zA-Z]\w*/ });
 
-const AdditionOperator = createToken({name: "AdditionOperator", pattern: Lexer.NA});
-const Plus = createToken({name: "Plus", pattern: /\+/, categories: AdditionOperator});
-const Minus = createToken({name: "Minus", pattern: /-/, categories: AdditionOperator});
+const AdditionOperator = createToken({
+  name: 'AdditionOperator',
+  pattern: Lexer.NA,
+});
+const Plus = createToken({
+  name: 'Plus',
+  pattern: /\+/,
+  categories: AdditionOperator,
+});
+const Minus = createToken({
+  name: 'Minus',
+  pattern: /-/,
+  categories: AdditionOperator,
+});
 
-const MultiplicationOperator = createToken({name: "MultiplicationOperator", pattern: Lexer.NA});
-const Multi = createToken({name: "Multi", pattern: /\*/, categories: MultiplicationOperator});
-const Div = createToken({name: "Div", pattern: /\//, categories: MultiplicationOperator});
+const MultiplicationOperator = createToken({
+  name: 'MultiplicationOperator',
+  pattern: Lexer.NA,
+});
+const Multi = createToken({
+  name: 'Multi',
+  pattern: /\*/,
+  categories: MultiplicationOperator,
+});
+const Div = createToken({
+  name: 'Div',
+  pattern: /\//,
+  categories: MultiplicationOperator,
+});
 
-const PowerOperator = createToken({name: "PowerOperator", pattern: /\^/});
+const PowerOperator = createToken({ name: 'PowerOperator', pattern: /\^/ });
 
-const LParen = createToken({name: "LParen", pattern: /\(/});
-const RParen = createToken({name: "RParen", pattern: /\)/});
-const NumberLiteral = createToken({name: "NumberLiteral", pattern: /[1-9]\d*/});
+const LParen = createToken({ name: 'LParen', pattern: /\(/ });
+const RParen = createToken({ name: 'RParen', pattern: /\)/ });
+const NumberLiteral = createToken({
+  name: 'NumberLiteral',
+  pattern: /[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?/,
+});
 
-const PowerFunc = createToken({name: "PowerFunc", pattern: /power/});
-const Comma = createToken({name: "Comma", pattern: /,/});
+const PowerFunc = createToken({ name: 'PowerFunc', pattern: /power/ });
+const Comma = createToken({ name: 'Comma', pattern: /,/ });
 
 const allTokens = [
   // line break must be before whitespace
@@ -38,20 +64,20 @@ const allTokens = [
   WhiteSpace,
 
   // Operators (from lower precedence to higher precendence)
-  Plus, 
-  Minus, 
-  Multi, 
-  Div, 
-  LParen, 
-  RParen, 
-  NumberLiteral, 
+  Plus,
+  Minus,
+  Multi,
+  Div,
+  LParen,
+  RParen,
+  NumberLiteral,
   AssignOperator,
-  AdditionOperator, 
-  MultiplicationOperator, 
+  AdditionOperator,
+  MultiplicationOperator,
   PowerOperator,
 
   // functions
-  PowerFunc, 
+  PowerFunc,
 
   // identifier
   Variable,
@@ -71,96 +97,100 @@ class CalculatorParser extends CstParser {
     this.performSelfAnalysis();
   }
 
-  public lines = this.RULE("lines", () => {
+  public lines = this.RULE('lines', () => {
     this.MANY_SEP({
       SEP: NewLine,
-      DEF: () => { 
+      DEF: () => {
         this.SUBRULE2(this.line);
-      }
+      },
     });
   });
 
-  public line = this.RULE("line", () => {
+  private line = this.RULE('line', () => {
     this.OPTION(() => {
       this.OR([
         { ALT: () => this.SUBRULE(this.assignOperation) },
-        { ALT: () => this.SUBRULE(this.expression) },
+        { ALT: () => this.SUBRULE(this.additionExpression) },
       ]);
     });
   });
 
-  private assignOperation = this.RULE("assignOperation", () => {
+  private assignOperation = this.RULE('assignOperation', () => {
     this.CONSUME(Variable);
     this.CONSUME(AssignOperator);
-    this.SUBRULE(this.expression);
-  });
-
-  public expression = this.RULE("expression", () => {
-    this.SUBRULE(this.additionExpression)
+    this.SUBRULE(this.additionExpression);
   });
 
   //  lowest precedence thus it is first in the rule chain
   // The precedence of binary expressions is determined by how far down the Parse Tree
   // The binary expression appears.
-  private additionExpression = this.RULE("additionExpression", () => {
-    this.SUBRULE(this.multiplicationExpression, {LABEL: "lhs"});
+  private additionExpression = this.RULE('additionExpression', () => {
+    this.SUBRULE(this.multiplicationExpression, { LABEL: 'lhs' });
     this.MANY(() => {
       // consuming 'AdditionOperator' will consume either Plus or Minus as they are subclasses of AdditionOperator
       this.CONSUME(AdditionOperator);
       //  the index "2" in SUBRULE2 is needed to identify the unique position in the grammar during runtime
-      this.SUBRULE2(this.multiplicationExpression, {LABEL: "rhs"});
+      this.SUBRULE2(this.multiplicationExpression, { LABEL: 'rhs' });
     });
   });
 
-  private multiplicationExpression = this.RULE("multiplicationExpression", () => {
-    this.SUBRULE(this.negationExpression, {LABEL: "lhs"});
-    this.MANY(() => {
-      this.CONSUME(MultiplicationOperator);
-      //  the index "2" in SUBRULE2 is needed to identify the unique position in the grammar during runtime
-      this.SUBRULE2(this.negationExpression, {LABEL: "rhs"});
-    });
-  });
+  private multiplicationExpression = this.RULE(
+    'multiplicationExpression',
+    () => {
+      this.SUBRULE(this.negationExpression, { LABEL: 'lhs' });
+      this.MANY(() => {
+        this.CONSUME(MultiplicationOperator);
+        //  the index "2" in SUBRULE2 is needed to identify the unique position in the grammar during runtime
+        this.SUBRULE2(this.negationExpression, { LABEL: 'rhs' });
+      });
+    },
+  );
 
-  private negationExpression = this.RULE("negationExpression", () => {
-    this.OPTION(() => this.CONSUME(Minus, { LABEL: "negation" }));
-    this.SUBRULE(this.powerExpression, { LABEL: "value" });
+  private negationExpression = this.RULE('negationExpression', () => {
+    this.OPTION(() => this.CONSUME(Minus, { LABEL: 'negation' }));
+    this.SUBRULE(this.powerExpression, { LABEL: 'value' });
   });
 
   // Can only negatve atomic expressions. Used for exponent of power expression
-  private negationAtomicExpression = this.RULE("negationAtomicExpression", () => {
-    this.OPTION(() => this.CONSUME(Minus, { LABEL: "negation" }));
-    this.SUBRULE(this.atomicExpression, { LABEL: "value" });
-  });
+  private negationAtomicExpression = this.RULE(
+    'negationAtomicExpression',
+    () => {
+      this.OPTION(() => this.CONSUME(Minus, { LABEL: 'negation' }));
+      this.SUBRULE(this.atomicExpression, { LABEL: 'value' });
+    },
+  );
 
-  private powerExpression = this.RULE("powerExpression", () => {
-    this.SUBRULE(this.atomicExpression, { LABEL: "base" });
+  private powerExpression = this.RULE('powerExpression', () => {
+    this.SUBRULE(this.atomicExpression, { LABEL: 'base' });
     this.MANY(() => {
       this.CONSUME(PowerOperator);
-      this.SUBRULE2(this.negationAtomicExpression, { LABEL: "exponent" });
+      this.SUBRULE2(this.negationAtomicExpression, { LABEL: 'exponent' });
     });
   });
 
-  private atomicExpression = this.RULE("atomicExpression", () => this.OR([
-    // parenthesisExpression has the highest precedence and thus it appears
-    // in the "lowest" leaf in the expression ParseTree.
-    {ALT: () => this.SUBRULE(this.parenthesisExpression)},
-    {ALT: () => this.CONSUME(NumberLiteral)},
-    {ALT: () => this.SUBRULE(this.powerFunction)},
-    {ALT: () => this.CONSUME(Variable)},
-  ]));
+  private atomicExpression = this.RULE('atomicExpression', () =>
+    this.OR([
+      // parenthesisExpression has the highest precedence and thus it appears
+      // in the "lowest" leaf in the expression ParseTree.
+      { ALT: () => this.SUBRULE(this.parenthesisExpression) },
+      { ALT: () => this.CONSUME(NumberLiteral) },
+      { ALT: () => this.SUBRULE(this.powerFunction) },
+      { ALT: () => this.CONSUME(Variable) },
+    ]),
+  );
 
-  private parenthesisExpression = this.RULE("parenthesisExpression", () => {
+  private parenthesisExpression = this.RULE('parenthesisExpression', () => {
     this.CONSUME(LParen);
-    this.SUBRULE(this.expression);
+    this.SUBRULE(this.additionExpression);
     this.CONSUME(RParen);
   });
 
-  private powerFunction = this.RULE("powerFunction", () => {
+  private powerFunction = this.RULE('powerFunction', () => {
     this.CONSUME(PowerFunc);
     this.CONSUME(LParen);
-    this.SUBRULE(this.expression, {LABEL: "base"});
+    this.SUBRULE(this.atomicExpression, { LABEL: 'base' });
     this.CONSUME(Comma);
-    this.SUBRULE2(this.expression, {LABEL: "exponent"});
+    this.SUBRULE2(this.atomicExpression, { LABEL: 'exponent' });
     this.CONSUME(RParen);
   });
 }
@@ -169,16 +199,15 @@ class CalculatorParser extends CstParser {
 // reuse the same parser instance.
 export const parser = new CalculatorParser();
 
-
 // ----------------- Interpreter -----------------
-const BaseCstVisitor = parser.getBaseCstVisitorConstructor()
+const BaseCstVisitor = parser.getBaseCstVisitorConstructor();
 
 class CalculatorInterpreter extends BaseCstVisitor {
   private stack = new Map();
-  
+
   constructor() {
-    super()
-    this.validateVisitor()
+    super();
+    this.validateVisitor();
   }
 
   lines(ctx) {
@@ -187,8 +216,8 @@ class CalculatorInterpreter extends BaseCstVisitor {
   }
 
   line(ctx) {
-    if (ctx.expression) {
-      return this.visit(ctx.expression);
+    if (ctx.additionExpression) {
+      return this.visit(ctx.additionExpression);
     } else if (ctx.assignOperation) {
       return this.visit(ctx.assignOperation);
     } else {
@@ -198,74 +227,77 @@ class CalculatorInterpreter extends BaseCstVisitor {
 
   assignOperation(ctx) {
     const variable_name = ctx.Variable[0].image;
-    const result = this.visit(ctx.expression);
+    const result = this.visit(ctx.additionExpression);
     this.stack[variable_name] = result;
     return result;
   }
 
-  expression(ctx) {
-    return this.visit(ctx.additionExpression)
-  }
-
   additionExpression(ctx) {
-    let result = this.visit(ctx.lhs)
+    let result: BigNumber = this.visit(ctx.lhs);
 
     // "rhs" key may be undefined as the grammar defines it as optional (MANY === zero or more).
     if (ctx.rhs) {
       ctx.rhs.forEach((rhsOperand, idx) => {
         // there will be one operator for each rhs operand
-        let rhsValue = this.visit(rhsOperand)
-        let operator = ctx.AdditionOperator[idx]
+        let rhsValue = this.visit(rhsOperand);
+        let operator = ctx.AdditionOperator[idx];
 
         if (tokenMatcher(operator, Plus)) {
-          result += rhsValue
+          result = result.plus(rhsValue);
         } else {
           // Minus
-          result -= rhsValue
+          result = result.minus(rhsValue);
         }
-      })
+      });
     }
 
-    return result
+    return result;
   }
 
   multiplicationExpression(ctx) {
-    let result = this.visit(ctx.lhs)
+    let result: BigNumber = this.visit(ctx.lhs);
     if (ctx.rhs) {
       ctx.rhs.forEach((rhsOperand, idx) => {
         // there will be one operator for each rhs operand
-        let rhsValue = this.visit(rhsOperand)
-        let operator = ctx.MultiplicationOperator[idx]
+        let rhsValue = this.visit(rhsOperand);
+        let operator = ctx.MultiplicationOperator[idx];
 
         if (tokenMatcher(operator, Multi)) {
-          result *= rhsValue
+          result = result.multipliedBy(rhsValue);
         } else {
           // Division
-          result /= rhsValue
+          result = result.dividedBy(rhsValue);
         }
-      })
+      });
     }
 
-    return result
+    return result;
   }
 
   negationExpression(ctx) {
-    const result = this.visit(ctx.value);
-    return ctx.negation ? -result : result;
+    const result: BigNumber = this.visit(ctx.value);
+    return ctx.negation ? result.negated() : result;
   }
 
   negationAtomicExpression(ctx) {
-    const result = this.visit(ctx.value);
-    return ctx.negation ? -result : result;
+    const result: BigNumber = this.visit(ctx.value);
+    return ctx.negation ? result.negated() : result;
   }
 
   powerExpression(ctx) {
-    let base = this.visit(ctx.base);
+    let base: BigNumber = this.visit(ctx.base);
 
     if (ctx.exponent) {
       ctx.exponent.reverse().forEach((expNode) => {
         const exp = this.visit(expNode);
-        base = Math.pow(base, exp)
+        try {
+          // BigNumber pow method only accept int as exponent
+          base = base.pow(exp);
+        } catch (e) {
+          // walk around for float exponent
+          const n = Math.pow(base.toNumber(), exp.toNumber());
+          base = new BigNumber(n);
+        }
       });
     }
 
@@ -276,27 +308,27 @@ class CalculatorInterpreter extends BaseCstVisitor {
     if (ctx.parenthesisExpression) {
       // passing an array to "this.visit" is equivalent
       // to passing the array's first element
-      return this.visit(ctx.parenthesisExpression)
+      return this.visit(ctx.parenthesisExpression);
     } else if (ctx.NumberLiteral) {
       // If a key exists on the ctx, at least one element is guaranteed
-      return parseInt(ctx.NumberLiteral[0].image, 10)
+      return new BigNumber(ctx.NumberLiteral[0].image);
     } else if (ctx.powerFunction) {
-      return this.visit(ctx.powerFunction)
+      return this.visit(ctx.powerFunction);
     } else if (ctx.Variable) {
-      return this.stack[ctx.Variable[0].image]
+      return this.stack[ctx.Variable[0].image];
     }
   }
 
   parenthesisExpression(ctx) {
     // The ctx will also contain the parenthesis tokens, but we don't care about those
     // in the context of calculating the result.
-    return this.visit(ctx.expression)
+    return this.visit(ctx.additionExpression);
   }
 
   powerFunction(ctx) {
     const base = this.visit(ctx.base);
     const exponent = this.visit(ctx.exponent);
-    return Math.pow(base, exponent)
+    return Math.pow(base, exponent);
   }
 }
 
